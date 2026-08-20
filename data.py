da8f1165b163867e038531c2c13bbcc9d3f82ac9
MrHOMERO <carlosmartinezdemaria@gmail.com>
2026-08-19 23:56:18 -0300

    Create data.py

---
+++ data.py
@@ -0,0 +1,16 @@
+# Data oficial de la Temporada 2026 de Fórmula 1 (11 Escuderías / 22 Pilotos)
+PARRILLA_2026 = {
+    "McLaren": ["Lando Norris", "Oscar Piastri"],
+    "Ferrari": ["Charles Leclerc", "Lewis Hamilton"],
+    "Mercedes": ["George Russell", "Kimi Antonelli"],
+    "Red Bull Racing": ["Max Verstappen", "Isack Hadjar"],
+    "Williams": ["Alex Albon", "Carlos Sainz"],
+    "Audi": ["Nico Hülkenberg", "Gabriel Bortoleto"],
+    "Alpine": ["Pierre Gasly", "Franco Colapinto"],
+    "Haas": ["Oliver Bearman", "Esteban Ocon"],
+    "Racing Bulls": ["Liam Lawson", "Arvid Lindblad"],
+    "Aston Martin": ["Fernando Alonso", "Lance Stroll"],
+    "Cadillac": ["Sergio Pérez", "Valtteri Bottas"]
+}
+
+PUNTOS_SISTEMA = {1: 25, 2: 18, 3: 15, 4: 12, 5: 10, 6: 8, 7: 6, 8: 4, 9: 2, 10: 1}
